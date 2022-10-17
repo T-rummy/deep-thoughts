c942aa315fe5ec830d2f6a72a77da06aa8e612b8
Ruminert <ruminertt@Tanners-MacBook-Air.local>
2022-10-17 11:00:57 -0500

    added concurrently

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+const path = require('path');
+
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -23,6 +25,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
   
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
